dd39a24cf40ff36f72c325d57b947263489bc5e0
unknown <ononobip@gmail.com>
2022-09-28 23:21:46 +0100

    first commit

---
+++ src/Tours.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({tours}) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="underline"></div>
+      </div>
+      {tours.map((tour) => {
+
+        return <Tour key={tour.id} {...tour}></Tour>
+      })}
+    </section>
+  );
+};
+
+export default Tours;
