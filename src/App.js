2daa24e172d21c1cd1465185b367d22b846f8575
unknown <ononobip@gmail.com>
2022-10-01 22:39:52 +0100

    updated Tour Component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
   const [loading, setLoading] = useState(false)
   const [tours, setTours] = useState([])
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => (tour.id != id))
+    setTours(newTours)
+  }
+
+
   const fetchTours = async () => {
     setLoading(true);
 
@@ -29,9 +35,19 @@ function App() {
       </main>
     )
   }
+  if(tours.length === 0){
+    return(
+      <main>
+        <div className='title'>
+          <h2>No tours Left</h2>
+          <button onClick={fetchTours} className='btn'>Refresh</button>
+        </div>
+      </main>
+    )
+  }
   return(
     <main>
-      <Tours tours={tours}/> 
+      <Tours tours={tours} removeTour={removeTour}/> 
     </main>
   )
 }
